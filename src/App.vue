dc60f653ab66186a0369f530b1ce347fbcf6b1d8
TeLoeardBruh <damnloardbruh@gmail.com>
2023-03-02 15:24:58 +0700

    feat: resuable table

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,36 @@
 <template>
-  <TableComponent />
+  <TableComponent :table-data="tableData" />
+  <!-- as for pass props here we are using ':' because we wanted to be dynamic
+  props when we are passing on -->
 </template>
 
 <script lang="ts" setup>
 import TableComponent from './components/TableComponent.vue';
+
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+    age: 10,
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+    age: 10,
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+    age: 10,
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+    age: 10,
+  },
+];
 </script>
--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -1,32 +1,21 @@
 <template>
   <el-table :data="tableData" style="width: 100%">
-    <el-table-column prop="date" label="Date" width="180" />
-    <el-table-column prop="name" label="Name" width="180" />
-    <el-table-column prop="address" label="Address" />
+    <template v-for="value in headerTable" ::key="value.name">
+      <el-table-column :prop="value" :label="value" width="180" />
+    </template>
   </el-table>
 </template>
 
 <script lang="ts" setup>
-const tableData = [
-  {
-    date: '2016-05-03',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-02',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-04',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-];
+import { defineProps, onMounted, ref } from 'vue';
+
+const props = defineProps<{
+  tableData: any;
+}>();
+
+const headerTable = ref();
+
+onMounted(() => {
+  headerTable.value = Object.keys(props.tableData[0]);
+});
 </script>
